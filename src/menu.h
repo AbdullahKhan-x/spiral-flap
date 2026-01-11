42a42dd7df3e8f1b78964e6deac16c26c74f6481
Muhammad Aamir Saifi <rmaamir2005@gmail.com>
2026-01-12 04:05:46 +0500

    Final

---
+++ src/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+void drawMainMenu(int screenWidth, int screenHeight, int highScore);
+void drawGameOverScreen(int screenWidth, int screenHeight, int score, int highScore);
+void drawPlayingUI(int screenWidth, int screenHeight, int score, int highScore);
+
+#endif
